3f82ff87ae0f3d92d2f505855dc81bbe213055bf
Filip Rybiński <rybciaw77@gmail.com>
2024-03-09 19:01:38 +0100

    TMP LandingPage

--- src/ClientApp/MyApp/src/app/app.component.ts
+++ src/ClientApp/MyApp/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'MyApp';
+  currentTime!: Date;
+  private timerSubscription: Subscription;
+  constructor() {
+    this.timerSubscription = interval(1000).subscribe(() => (this.currentTime = new Date()));
+  }
+
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
+  }
 }
