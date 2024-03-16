121688f1c91ad3b3210d79b8ec0b3e274f8df659
Filip Rybiński <rybciaw77@gmail.com>
2024-03-16 19:07:56 +0100

    angular material

--- src/ClientApp/MyApp/src/app/app.module.ts
+++ src/ClientApp/MyApp/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
