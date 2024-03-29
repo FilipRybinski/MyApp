32ef2b4d36636ded0c3703929770b1af91d132f1
Filip Rybiński <rybciaw77@gmail.com>
2024-03-29 23:12:36 +0100

    remove landing page && add new favicon.ico

--- src/ClientApp/MyApp/src/app/app.module.ts
+++ src/ClientApp/MyApp/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material.module';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/ClientApp/MyApp/src/app/components/landing-page/landing-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss',
-})
-export class LandingPageComponent {}
