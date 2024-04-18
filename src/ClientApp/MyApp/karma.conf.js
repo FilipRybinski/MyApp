9e04449e2d1c6b8c09122abaace31f30f547b4bb
Filip Rybiński <rybciaw77@gmail.com>
2024-04-18 20:26:21 +0200

    add single run for karma && check before commit script

--- src/ClientApp/MyApp/karma.conf.js
+++ src/ClientApp/MyApp/karma.conf.js
@@ -1,9 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
+    singleRun: true,
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
