d0fb8ae4d82d6de7dd92d1d98870d760db1ee14c
Filip Rybiński <rybciaw77@gmail.com>
2024-05-11 20:55:26 +0200

    with credentials

--- src/MyApp.Infrastructure/Extensions.cs
+++ src/MyApp.Infrastructure/Extensions.cs
@@ -31,10 +31,11 @@ public static class Extensions
         app.UseAuthentication();
         app.UseAuthorization();
         app.MapControllers();
-        app.UseCors(x => x.AllowAnyOrigin()
+        app.UseCors(x => x.WithOrigins(CorsOptions.ConnectionUrl)
             .AllowAnyHeader()
             .WithMethods(CorsOptions.AllowedMethods)
-            .SetIsOriginAllowed(origin => true));
+            .SetIsOriginAllowed(origin => origin.StartsWith(CorsOptions.ConnectionUrl))
+            .AllowCredentials());
         
         return app;
     }
