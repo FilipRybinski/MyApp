b1506770410eb797119dd8bfcd21e5692605eedc
Filip Rybiński <rybciaw77@gmail.com>
2024-08-04 23:29:14 +0200

    add animation

--- src/ClientApp/MyApp/src/app/app.component.ts
+++ src/ClientApp/MyApp/src/app/app.component.ts
@@ -8,11 +8,15 @@ import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 export class AppComponent implements AfterViewInit {
   title = 'MyAppZone';
   private readonly loadingScreen: string = 'loading-screen';
+  private readonly fadeOut: string = 'fade-out';
 
   constructor(private readonly renderer: Renderer2) {}
 
   public ngAfterViewInit(): void {
     const element = document.getElementById(this.loadingScreen);
-    this.renderer.removeChild(element!.parentElement, element);
+    this.renderer.addClass(element, this.fadeOut);
+    element!.addEventListener('transitionend', () => {
+      this.renderer.removeChild(element!.parentElement, element);
+    });
   }
 }
