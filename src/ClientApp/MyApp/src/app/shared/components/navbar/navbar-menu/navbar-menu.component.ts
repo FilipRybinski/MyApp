b7fd6beadf91c7bd9424d9b908ab954ec0cf5cbd
Filip Rybiński <rybciaw77@gmail.com>
2024-08-07 23:44:31 +0200

    fix

--- src/ClientApp/MyApp/src/app/shared/components/navbar/navbar-menu/navbar-menu.component.ts
+++ src/ClientApp/MyApp/src/app/shared/components/navbar/navbar-menu/navbar-menu.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { getHomeUrl, PATH } from '../../../../../constants/routing/path';
 import { AuthService } from '../../../../../service/auth/auth.service';
 import { Router } from '@angular/router';
@@ -8,7 +8,6 @@ import { SharedService } from '../../../service';
   selector: 'app-navbar-menu',
   templateUrl: './navbar-menu.component.html',
   styleUrl: './navbar-menu.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarMenuComponent {
   protected readonly PATH = PATH;
@@ -17,7 +16,11 @@ export class NavbarMenuComponent {
     public readonly authService: AuthService,
     private readonly sharedService: SharedService,
     private readonly router: Router
-  ) {}
+  ) {
+    effect(() => {
+      console.log(`The count is: ${this.authService.isAuth()}`);
+    });
+  }
 
   public logout(): void {
     this.sharedService.logout().subscribe({
