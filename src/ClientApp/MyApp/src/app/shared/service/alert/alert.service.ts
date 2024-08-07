4f201111136eacc20bec13012efd3b915ea2e0c6
Filip Rybiński <rybciaw77@gmail.com>
2024-08-07 23:22:38 +0200

    fix

--- src/ClientApp/MyApp/src/app/shared/service/alert/alert.service.ts
+++ src/ClientApp/MyApp/src/app/shared/service/alert/alert.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpErrorResponse } from '@angular/common/http';
+import { SnackBarConfig } from '../../../../constants/SnackBarConfig';
 
 @Injectable()
 export class AlertService {
   constructor(private snackBar: MatSnackBar) {}
 
   public handleSuccess(message: string) {
-    this.snackBar.open(message);
+    this.snackBar.open(message, undefined, SnackBarConfig);
   }
 
   public handleError(error: HttpErrorResponse) {
-    this.snackBar.open(this.extractError(error));
+    this.snackBar.open(this.extractError(error), undefined, SnackBarConfig);
   }
 
   private extractError(error: HttpErrorResponse): string {
