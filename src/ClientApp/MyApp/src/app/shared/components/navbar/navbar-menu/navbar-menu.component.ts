028292dff58f690c241581cafd15d155ae43c5a5
Filip Rybiński <rybciaw77@gmail.com>
2024-08-08 00:11:20 +0200

    fix

--- src/ClientApp/MyApp/src/app/shared/components/navbar/navbar-menu/navbar-menu.component.ts
+++ src/ClientApp/MyApp/src/app/shared/components/navbar/navbar-menu/navbar-menu.component.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { getHomeUrl, PATH } from '../../../../../constants/routing/path';
 import { AuthService } from '../../../../../service/auth/auth.service';
 import { Router } from '@angular/router';
 import { SharedService } from '../../../service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar-menu',
   templateUrl: './navbar-menu.component.html',
   styleUrl: './navbar-menu.component.scss',
 })
-export class NavbarMenuComponent {
+export class NavbarMenuComponent implements OnInit, OnDestroy {
   protected readonly PATH = PATH;
-  public isAuth: boolean = this.authService.isAuth();
+  public isAuth!: boolean;
+
+  private subscription$ = new Subscription();
 
   constructor(
     public readonly authService: AuthService,
     private readonly sharedService: SharedService,
     private readonly router: Router
   ) {}
+
+  public ngOnInit(): void {
+    this.subscription$.add(this.handleIsAuthChange());
+  }
 
   public logout(): void {
     this.sharedService.logout().subscribe({
@@ -26,5 +33,15 @@ export class NavbarMenuComponent {
           .navigate(getHomeUrl())
           .then(() => (this.authService.setAuthUser = null)),
     });
+  }
+
+  private handleIsAuthChange(): Subscription {
+    return this.authService.isAuthChanged$.subscribe(
+      isAuth => (this.isAuth = isAuth)
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
   }
 }
--- src/ClientApp/MyApp/src/service/auth/auth.service.ts
+++ src/ClientApp/MyApp/src/service/auth/auth.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
 import { User } from '../../interfaces/account/user';
+import { Subject } from 'rxjs';
 
 type AuthUserType = User | null;
 
 @Injectable()
 export class AuthService {
+  public isAuthChanged$: Subject<boolean> = new Subject<boolean>();
   public authUser: WritableSignal<AuthUserType> = signal<AuthUserType>(null);
   public isAuth: WritableSignal<boolean> = signal<boolean>(false);
 
   public set setAuthUser(user: AuthUserType) {
     this.isAuth.set(!!user);
+    this.isAuthChanged$.next(!!user);
     this.authUser.set(user);
   }
 }
