3d57fa8e98e96f362b4f5e7fcda6e70f542f482b
Filip Rybiński <rybciaw77@gmail.com>
2024-09-04 20:50:32 +0200

    improve loading screen

--- src/ClientApp/MyApp/src/app/app.component.ts
+++ src/ClientApp/MyApp/src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { AfterViewInit, Component, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'MyAppZone';
-  private readonly loadingScreen: string = 'loading-screen';
-  private readonly fadeOut: string = 'fade-out';
-
-  constructor(private readonly renderer: Renderer2) {}
-
-  public ngAfterViewInit(): void {
-    const element = document.getElementById(this.loadingScreen);
-    this.renderer.addClass(element, this.fadeOut);
-    element!.addEventListener('transitionend', () => {
-      this.renderer.removeChild(element!.parentElement, element);
-    });
-  }
 }
