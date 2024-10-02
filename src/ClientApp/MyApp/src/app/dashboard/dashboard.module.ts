ec4389a310a2b1b3413f01d04bc62fdb8d1709b0
FilipRybiski <rybciaw77@gmail.com>
2024-10-02 22:41:14 +0200

    add ngrx charts

--- src/ClientApp/MyApp/src/app/dashboard/dashboard.module.ts
+++ src/ClientApp/MyApp/src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import * as Pages from './pages';
+import * as Components from './components';
 import { SharedModule } from '../shared/shared.module';
-import { DashboardMenuComponent } from './components/dashboard-menu/dashboard-menu.component';
 
 @NgModule({
-  declarations: [Pages.DashboardComponent, DashboardMenuComponent],
+  declarations: [Pages.DashboardComponent, Components.DashboardMenuComponent],
   imports: [DashboardRoutingModule, SharedModule],
 })
 export class DashboardModule {}
