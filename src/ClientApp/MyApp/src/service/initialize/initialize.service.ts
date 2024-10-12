61833a3e2128ca49d3eaab447557698da720cf7d
Filip Rybiński <rybciaw77@gmail.com>
2024-10-12 17:30:54 +0200

    fix

--- src/ClientApp/MyApp/src/service/initialize/initialize.service.ts
+++ src/ClientApp/MyApp/src/service/initialize/initialize.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { firstValueFrom } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -6,6 +6,7 @@ import { User } from '../../interfaces/account/user';
 import { FeatureFlags } from '../../interfaces/featureFlags/featureFlags';
 import { attachInitialData } from '../../state/startup/startup.action';
 import { HttpClient } from '@angular/common/http';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -13,19 +14,22 @@ import { HttpClient } from '@angular/common/http';
 export class InitializeService {
   constructor(
     private readonly store: Store,
-    private readonly http: HttpClient
+    private readonly http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: NonNullable<unknown>
   ) {}
 
   public async initialize(): Promise<void> {
+    if (isPlatformServer(this.platformId)) {
+      return;
+    }
     try {
-      const [user, featureFlags] = await Promise.all([
-        firstValueFrom(
-          this.http.get<User>(environment.URL.USERS.IS_AUTHORIZED)
-        ),
-        firstValueFrom(
-          this.http.get<FeatureFlags>(environment.URL.FEATURE_FLAGS)
-        ),
-      ]);
+      const user = await firstValueFrom(
+        this.http.get<User>(environment.URL.USERS.IS_AUTHORIZED)
+      );
+      const featureFlags = await firstValueFrom(
+        this.http.get<FeatureFlags>(environment.URL.FEATURE_FLAGS)
+      );
+
       this.store.dispatch(attachInitialData({ user, featureFlags }));
     } catch (error) {
       console.error('Failed to initialize service');
