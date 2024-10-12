9831555c62193182c00e2cb62fe7f6e22f232fe5
Filip Rybiński <rybciaw77@gmail.com>
2024-10-12 18:27:23 +0200

    add loading animation

--- src/ClientApp/MyApp/src/app/app.component.ts
+++ src/ClientApp/MyApp/src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { handleLoadingAnimation } from '../utils/loading-animation.util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'MyAppZone';
+
+  constructor(@Inject(PLATFORM_ID) private platform: object) {}
+
+  public ngAfterViewInit(): void {
+    isPlatformBrowser(this.platform) && handleLoadingAnimation();
+  }
 }
---
+++ src/ClientApp/MyApp/src/constants/selectors/selectors-variable.ts
@@ -0,0 +1,3 @@
+export const BODY_SELECTOR = 'body';
+export const LOADING_ANIMATION_SELECTOR = 'loading-screen';
+export const LOADING_ANIMATION_CLASS = 'overflow-hidden';
