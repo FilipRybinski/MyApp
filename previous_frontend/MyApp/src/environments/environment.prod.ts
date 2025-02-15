9001ddc6728c7d6f63b5987d2c9fbb012392b3bc
Filip Rybiński <rybciaw77@gmail.com>
2025-02-16 00:41:37 +0100

    update urls

--- previous_frontend/MyApp/src/environments/environment.prod.ts
+++ previous_frontend/MyApp/src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
   production: true,
   URL: {
     USERS: {
-      SIGN_IN: `${DEFAULT_IDENTITY_URL}/Users/SignIn`,
-      SIGN_UP: `${DEFAULT_IDENTITY_URL}/Users/SignUp`,
-      IS_AUTHORIZED: `${DEFAULT_IDENTITY_URL}/Users/IsAuthorized`,
-      LOGOUT: `${DEFAULT_IDENTITY_URL}/Users/Logout`,
+      SIGN_IN: `${DEFAULT_IDENTITY_URL}/Identity/SignIn`,
+      SIGN_UP: `${DEFAULT_IDENTITY_URL}/Identity/SignUp`,
+      IS_AUTHORIZED: `${DEFAULT_IDENTITY_URL}/Identity/IsAuthorized`,
+      LOGOUT: `${DEFAULT_IDENTITY_URL}/Identity/Logout`,
     },
     FEATURE_FLAGS: `${DEFAULT_FEATURE_FLAGS_URL}/FeatureFlags/GetFeatureFlags`,
   },
