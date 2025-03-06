95950a654d57787d2b01a033d6de8fa27227aae6
Filip Rybiński <rybciaw77@gmail.com>
2025-03-06 23:14:35 +0100

    fix

--- microservices/Identity/Identity.Application/Queries/SignUp/SignUpHandler.cs
+++ microservices/Identity/Identity.Application/Queries/SignUp/SignUpHandler.cs
@@ -32,7 +32,7 @@ public sealed class SignUpHandler(
 
         var result = await userIdentityRepository.AddUserIdentityAsync(user);
         await requestHandler.SendRequestAsync<ConfirmationEmail, dynamic>(
-            "http://Queuemailer:5169/QueueMailer/SendConfirmationEmail",
+            "http://Queuemailer:8084/QueueMailer/SendConfirmationEmail",
             HttpMethod.Post,
             new ConfirmationEmail( user.Id, user.Email)
         );
