7199bb25fb01e2f74f775690b53a9d8775f8cade
FilipRybiski <rybciaw77@gmail.com>
2025-03-07 10:15:40 +0100

    fix

--- microservices/QueueMailer/QueueMailer.Api/Controllers/QueueMailerController.cs
+++ microservices/QueueMailer/QueueMailer.Api/Controllers/QueueMailerController.cs
@@ -1,29 +1,25 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using QueueMailer.Application.Commands.SendConfirmationEmail;
 using QueueMailer.Application.Commands.SendResetPasswordEmail;
 using Shared.Core.Abstractions;
-using Shared.Core.Policies;
 
 namespace QueueMailer.Api.Controllers;
 
 [ApiController]
 [Route("[controller]/[action]")]
-[Authorize(Policy = AuthPolicies.Internal)]
 public class QueueMailerController(
     ILogger<QueueMailerController> logger,
     ICommandHandler<ConfirmationEmail> confirmationEmailHandler,
     ICommandHandler<ResetPasswordEmail> resetPasswordEmailHandler
-    ) : ControllerBase
+) : ControllerBase
 {
-    
     [HttpPost]
     public async Task<ActionResult> SendConfirmationEmail(ConfirmationEmail command)
     {
         await confirmationEmailHandler.HandleAsync(command);
         return Ok();
     }
-    
+
     [HttpPost]
     public async Task<ActionResult> SendResetPasswordEmail(ResetPasswordEmail command)
     {
