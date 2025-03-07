f8f05986d0b3c4088ce43a87ea9271e87879d42e
Filip Rybiński <rybciaw77@gmail.com>
2025-03-07 21:26:59 +0100

    Add yarp proxy

--- microservices/Gateway/Gateway.Api/Program.cs
+++ microservices/Gateway/Gateway.Api/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
