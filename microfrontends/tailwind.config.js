001bb21cb04e709bed49ebf44e5934fcc96e33bc
FilipRybiski <rybciaw77@gmail.com>
2025-04-14 22:25:55 +0200

    add typho and colors

--- microfrontends/tailwind.config.js
+++ microfrontends/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [    
+  content: [
     './apps/**/*.{html,ts}',
-    './libs/**/*.{html,ts}', 
+    './libs/**/*.{html,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        background: {
+          DEFAULT: '#131617',
+          light: '#16191b'
+        },
+        green:{
+          DEFAULT: '#057d52',
+          light: '#339f4a'
+        }
+      }
+    },
   },
   plugins: [],
 }
