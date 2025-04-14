98385e22aee32df22978ab4df00368658f1a37d9
Filip Rybiński <rybciaw77@gmail.com>
2025-04-14 21:31:40 +0200

    add tailwind

--- microfrontends/apps/identity/src/app/app.component.ts
+++ microfrontends/apps/identity/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ RouterModule, MatButtonModule, MatProgressSpinnerModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ microfrontends/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [    
+    './apps/**/*.{html,ts}',
+    './libs/**/*.{html,ts}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
