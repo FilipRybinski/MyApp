529c24c4623177e8d14e6d29db79d2570b535704
Filip Rybiński <rybciaw77@gmail.com>
2025-05-29 22:54:23 +0200

    fix

--- microservices/Shared/Shared.Infrastructure/Exceptions/Middleware/ExceptionMiddleware.cs
+++ microservices/Shared/Shared.Infrastructure/Exceptions/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Shared.Core.Exceptions;
+using Shared.Core.Objects;
 
 namespace Shared.Infrastructure.Exceptions.Middleware;
 
@@ -23,14 +24,13 @@ internal sealed class ExceptionMiddleware(ILogger<ExceptionMiddleware> logger) :
         logger.LogError(exception, "Exception thrown while processing request at {Path}", context.Request?.Path);
         var (statusCode, error) = exception switch
         {
-            CustomException => (StatusCodes.Status400BadRequest, new Error(exception
-                .GetType().Name.Replace("Exception", string.Empty), exception.Message)),
-            _ => (StatusCodes.Status500InternalServerError, new Error("Error", "There was an error"))
+            CustomException => (StatusCodes.Status400BadRequest, Result.Failure(Error.BadRequest($"{exception
+                .GetType().Name.Replace("Exception", string.Empty)} - ${exception.Message}"))),
+            _ => (StatusCodes.Status500InternalServerError, Result.Failure(Error.InternalServerError("Internal Server Error")))
         };
 
         context.Response.StatusCode = statusCode;
         await context.Response.WriteAsJsonAsync(error);
     }
 
-    private record Error(string Code, string Reason);
 }
